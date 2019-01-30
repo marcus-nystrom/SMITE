aaab0bc8213b6e61f8120e8dd38de5394f460015
marcus <marcus.h.nystrom@gmail.com>
2019-01-30 21:14:53 +0100

    Re-structure to pip package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SMITE',
+    url='https://github.com/marcus-nystrom/SMITE',
+    author='Marcus Nyström',
+    author_email='marcus.nystrom@humlab.lu.se',
+    packages=['smite'],
+    install_requires=['psychopy', 'tobii-research'],
+    version='0.1',
+    license='Creative Commons Attribution 4.0 (CC BY 4.0)',
+    description='SMITE is a toolbox for using eye trackers from SMI GmbH with Python, specifically offering integration with PsychoPy',
+    long_description=open('README.md').read(),
+)
