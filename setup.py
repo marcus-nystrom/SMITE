5835cd74fc206939865ba8041587b8852e332bde
marcus <marcus.h.nystrom@gmail.com>
2019-01-30 21:18:15 +0100

    Removed install requirement.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Marcus Nyström',
     author_email='marcus.nystrom@humlab.lu.se',
     packages=['smite'],
-    install_requires=['psychopy', 'tobii-research'],
+    install_requires=['psychopy'],
     version='0.1',
     license='Creative Commons Attribution 4.0 (CC BY 4.0)',
     description='SMITE is a toolbox for using eye trackers from SMI GmbH with Python, specifically offering integration with PsychoPy',
