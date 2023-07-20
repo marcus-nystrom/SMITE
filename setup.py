4244ee14ff1d6cb45a6561c9f3a347e4b8ea133d
mnystrom <marcus.nystrom@humlab.lu.se>
2023-07-20 11:08:43 +0200

    making a pypi package of it: py-smite

--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
 from setuptools import setup
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('smite/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setup(
-    name='SMITE',
-    url='https://github.com/marcus-nystrom/SMITE',
-    author='Marcus Nystrom',
-    author_email='marcus.nystrom@humlab.lu.se',
-    packages=['smite'],
-    install_requires=['psychopy'],
-    version='0.1',
-    license='Creative Commons Attribution 4.0 (CC BY 4.0)',
-    description='SMITE is a toolbox for using eye trackers from SMI GmbH with Python, specifically offering integration with PsychoPy',
+    name=main_ns['__title__'],
+    version=main_ns['__version__'],
+    author=main_ns['__author__'],
+    author_email=main_ns['__email__'],
+    description=main_ns['__description__'],
     long_description=open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url=main_ns['__url__'],
+    project_urls={
+        "Source Code": main_ns['__url__'],
+    },
+    license=main_ns['__license__'],
+
+    packages=['py-smite'],
+    python_requires="=2.7",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['numpy'],
 )
